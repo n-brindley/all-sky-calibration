132cc230722d948937c138c1b6a8b761bb583856
n-brindley <114927167+n-brindley@users.noreply.github.com>
2026-07-08 12:34:49 +0100

    Update STAR_CAL.py

--- STAR_CAL.py
+++ STAR_CAL.py
@@ -47,10 +47,10 @@ def gaussian_kernel(L=15, sigma=15/4):
     kernel = np.outer(gauss, gauss)
     return kernel / np.sum(kernel)
 
-def detrend_image(raw_im_file,L,SIG,Rlim = 2500,Blim = 10,IMTYPE = 'SCMOS',FROM_FILE = True):
+def detrend_image(raw_im_file,L,SIG,Rlim = 2500,Blim = 10,RGB_CONV = False,FROM_FILE = True):
     if FROM_FILE:
         test_im = plt.imread(raw_im_file)
-        if IMTYPE == 'ASC':
+        if RGB_CONV:
             test_im = rgb2gray(test_im)
     else:
         test_im = raw_im_file
@@ -450,14 +450,14 @@ def geo_zenith(popt,TYPE = None):
         
    
 composite = composite_image(r'/path/to/test_ims/')
-##detrend_im = detrend_image(r'/users/nick/documents/phd/codes/Sony2016-01-11/LYR-Sony-110116_190014.jpg',Rlim = 2250,Blim = 10,L = 50,SIG = 50/4)
+##detrend_im = detrend_image(r'/users/nick/documents/phd/codes/Sony2016-01-11/LYR-Sony-110116_190014.jpg',Rlim = 2250,Blim = 10,L = 50,SIG = 50/4,FROM_FILE = True,RGB_CONV = True)
 
 ### SCMOS EXAMPLE: this is a good fit for rectilinear: np.array([3.49200138e+02, 4.90383673e+02, 3.51607870e-01, 3.13864310e+00,3.02304340e+00, 3.94774814e+03])
 ### popt[0] = optical axis x coord; popt[1] = optical axis y coord; popt[2] = rotation1 ; popt[3] = rotation2; popt[4] = rotation3; popt[5] = focal length
 ### the above is a very good fit, but the optical axis seems a bit far from the centre of the image.
 
 ### when TYPE not in ['rectilinear','orthographic','equidistant'], popt[5] = focal length * k1; popt[6] = k2, where k1, k2 are lens parameters
-detrend_im = detrend_image(composite,Rlim = 500,Blim = 10,L = 20,SIG = 20/4,IMTYPE = 'SCMOS',FROM_FILE = False)
+detrend_im = detrend_image(composite,Rlim = 500,Blim = 10,L = 20,SIG = 20/4,RGB_CONV = False,FROM_FILE = False)
 
 sao_dir = r'/path/to/sao_dir/'
 cam_geodet_lat = 78.14796
